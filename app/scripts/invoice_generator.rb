e4b49a5e620d25cc3a9237c7e16ce66c8af531e9
VitalyKadyrko <wrather28@gmail.com>
2024-08-27 23:22:07 +0300

    create price for every cart product

--- app/scripts/invoice_generator.rb
+++ app/scripts/invoice_generator.rb
@@ -16,6 +16,12 @@ module InvoiceGenerator
         stripe_product = Stripe::Product.create({
           name: product_data['title']
         })
+
+        stripe_price = Stripe::Price.create({
+          product: stripe_product.id,
+          currency: 'usd',
+          unit_amount: product_data['price']
+        })
     end
   end
 end
