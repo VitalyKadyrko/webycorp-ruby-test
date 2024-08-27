c6cf2a87cfe7c9644621e960b74eeac34e56028f
VitalyKadyrko <wrather28@gmail.com>
2024-08-28 01:04:54 +0300

    config vcr

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 require 'vcr'
+require 'webmock/rspec'
 
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/vcr_cassettes'
   config.hook_into :faraday
-  config.filter_sensitive_data('<STRIPE_API_KEY>') { ENV.fetch('TEST_APP__STRIPE__API_KEY', nil) }
   config.configure_rspec_metadata!
+  config.filter_sensitive_data('<STRIPE_API_KEY>') { ENV['TEST_APP__STRIPE__API_KEY'] }
+  config.allow_http_connections_when_no_cassette = true
+  config.debug_logger = $stdout
 end
