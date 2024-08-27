83481e97d09ba4af16e1845763a0ed8091d76b3a
VitalyKadyrko <wrather28@gmail.com>
2024-08-27 22:30:31 +0300

    create method for get carts list

--- app/lib/fakestoreapi_service/client.rb
+++ app/lib/fakestoreapi_service/client.rb
@@ -6,6 +6,10 @@ module FakeStoreAPIService
     def initialize
       @connection = Faraday.new
     end
-    
+
+    def get_carts_list
+      response = @connection.get('https://fakestoreapi.com/carts')
+      JSON.parse(response.body)
+    end
   end
 end
