72b20b0a1bb0cfec317dbec9accdf95ca8e950d9
VitalyKadyrko <wrather28@gmail.com>
2024-08-28 00:53:23 +0300

    add loging request to fakestore client

--- app/lib/fakestoreapi_service/client.rb
+++ app/lib/fakestoreapi_service/client.rb
@@ -7,22 +7,33 @@ module FakeStoreAPIService
     BASE_URL = 'https://fakestoreapi.com'
 
     def initialize
-      @connection = Faraday.new(url: BASE_URL)
+      @connection = Faraday.new(url: BASE_URL) do |faraday|
+        faraday.response :logger, Application.logger, bodies: true
+        faraday.adapter Faraday.default_adapter
     end
 
     def get_carts_list
+      log_request('GET', '/carts')
       response = @connection.get('/carts')
       JSON.parse(response.body)
     end
 
     def get_user(_user_id)
+      log_request('GET', "/users/#{user_id}")
       response = @connection.get("users/#{user_id}")
       JSON.parse(response.body)
     end
 
     def get_product(_product_id)
+      log_request('GET', "/products/#{product_id}")
       response = @connection.get("/products/#{product_id}")
       JSON.parse(response.body)
+    end
+
+    private
+
+    def log_request(method, endpoint)
+      Application.logger.info("FakeStoreAPI Request: #{method} #{endpoint}")
     end
   end
 end
