95777154c849198a85549a6c1ab28da75e22d552
VitalyKadyrko <wrather28@gmail.com>
2024-08-28 00:39:36 +0300

    refactor fakestoreapi client

--- app/lib/fakestoreapi_service/client.rb
+++ app/lib/fakestoreapi_service/client.rb
@@ -2,23 +2,24 @@ require 'faraday'
 
 module FakeStoreAPIService
   class Client
+    BASE_URL = 'https://fakestoreapi.com'.freeze
 
     def initialize
-      @connection = Faraday.new
+      @connection = Faraday.new(url: BASE_URL)
     end
 
     def get_carts_list
-      response = @connection.get('https://fakestoreapi.com/carts')
+      response = @connection.get('/carts')
       JSON.parse(response.body)
     end
 
     def get_user(user_id)
-      response = @connection.get("https://fakestoreapi.com/users/#{user_id}")
+      response = @connection.get('users/#{user_id}')
       JSON.parse(response.body)
     end
 
     def get_product(product_id)
-      response = @connection.get("https://fakestoreapi.com/products/#{product_id}")
+      response = @connection.get('/products/#{product_id}')
       JSON.parse(response.body)
     end
   end
